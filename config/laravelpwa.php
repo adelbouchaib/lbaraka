da2e9a944c74b1acf908027728bdf6517ff3e60c
Adel-Bouchaib <adelbouchaib@outlook.com>
2025-04-07 10:07:47 +0100

    v.0.1.2 after pwa 2

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'name' => 'LaravelPWA',
+    'name' => 'Supplaio',
     'manifest' => [
-        'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'name' => env('APP_NAME', 'Supplaio'),
+        'short_name' => 'SUP',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
