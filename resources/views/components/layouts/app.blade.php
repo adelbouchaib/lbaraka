787fe0e3a88e183873c969916f77431bc2086650
Adel-Bouchaib <adelbouchaib@outlook.com>
2025-04-07 10:40:06 +0100

    v.0.1.2 new pwa

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,11 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- PWA  -->
-<meta name="theme-color" content="#6777ef"/>
-<link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
-<link rel="manifest" href="{{ asset('/manifest.json') }}">
-
         <style>
 
         @import url('https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&family=Noto+Kufi+Arabic:wght@100..900&display=swap');
@@ -19,7 +14,11 @@
 }
             </style>
 
+@PwaHead <!-- Add this directive to include the PWA meta tags -->
+
+
 <title>@yield('title', 'Default Title')</title>
+
 
         <!-- <title>{{ $title ?? 'Supplaio' }}</title> -->
         <link rel="icon" type="image/png" href="{{ asset('images/icon.png') }}">
@@ -39,23 +38,7 @@
         @livewireScripts
         @livewire('partials.footer')
 
-        <script src="{{ asset('/sw.js') }}"></script>
-<script>
-   if ("serviceWorker" in navigator) {
-      // Register a service worker hosted at the root of the
-      // site using the default scope.
-      navigator.serviceWorker.register("/sw.js").then(
-      (registration) => {
-         console.log("Service worker registration succeeded:", registration);
-      },
-      (error) => {
-         console.error(`Service worker registration failed: ${error}`);
-      },
-    );
-  } else {
-     console.error("Service workers are not supported.");
-  }
-</script>
+        @RegisterServiceWorkerScript <!-- This registers the service worker -->
 
     </body>
 </html>
