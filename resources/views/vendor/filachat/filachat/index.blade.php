08cce5c816aa15857f279c975231a8bfc97f0aef
Adel-Bouchaib <adelbouchaib@outlook.com>
2025-04-09 16:07:55 +0100

    v.0.1.3 notification 5

--- resources/views/vendor/filachat/filachat/index.blade.php
+++ resources/views/vendor/filachat/filachat/index.blade.php
@@ -11,6 +11,26 @@
 @script
 
 <script>
+    if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+              console.log("Service worker registration succeeded:", registration);
+          },
+          (error) => {
+              console.error(`Service worker registration failed: ${error}`);
+          },
+          );
+          
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Firebase Service Worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+
+   
+}
+
   const firebaseConfig = {
     apiKey: "AIzaSyCzz91VFPinYPTQ97Gjoq_lkGObCWib_88",
     authDomain: "lbaraka-1f464.firebaseapp.com",
