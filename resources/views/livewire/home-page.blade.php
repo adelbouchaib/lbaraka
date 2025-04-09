657470e2ecc7e1c994e40ae9f54764f6aa4b2ea3
Adel-Bouchaib <adelbouchaib@outlook.com>
2025-04-09 15:29:44 +0100

    v.0.1.3 notification 3

--- resources/views/livewire/home-page.blade.php
+++ resources/views/livewire/home-page.blade.php
@@ -233,3 +233,67 @@
 </section>
 
 </div>
+
+
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyCzz91VFPinYPTQ97Gjoq_lkGObCWib_88",
+    authDomain: "lbaraka-1f464.firebaseapp.com",
+    projectId: "lbaraka-1f464",
+    storageBucket: "lbaraka-1f464.firebasestorage.app",
+    messagingSenderId: "825065799200",
+    appId: "1:825065799200:web:e790fe16dc95fef0c50645",
+    measurementId: "G-JRRLJHPNCX"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const messaging = firebase.messaging();
+
+  let currentToken = null; // Declare a global variable
+
+
+
+  // Ask permission and get token
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      messaging.getToken({ vapidKey: 'BLX4N79hrhWKADdk6elMxsY9nijOccotAwR0mtsv00A8WtAtjK-LRqeR64uCLBNY0RlYCfVy8c5c0n3bnntfsiY' })
+        .then((currentToken) => {
+          if (currentToken) {
+            console.log("FCM Token:", currentToken);
+
+            // Send this token to your server
+            sendTokenToServer(currentToken); // Example: send the token to your server
+
+          } else {
+            console.log("No registration token available.");
+          }
+        }).catch((err) => {
+          console.error("Error getting token:", err);
+        });
+    } else {
+      console.warn("Permission not granted");
+    }
+  });
+
+
+  function sendTokenToServer(currentToken) {
+        
+        // Call the Livewire method and pass the JavaScript variable
+        @this.call('storeUserToken', currentToken);
+    }
+
+
+  // Foreground notification handler
+  messaging.onMessage((payload) => {
+    // console.log("Foreground message:", payload);
+    new Notification(payload.notification.title, {
+      body: payload.notification.body,
+    //   icon: '/icon.png'
+    });
+  });
+
+
+ 
+</script>
