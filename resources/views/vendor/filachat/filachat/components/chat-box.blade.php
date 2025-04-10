d664923571b0f42778b21531e85f36045dfa1892
Adel-Bouchaib <adelbouchaib@outlook.com>
2025-04-10 11:56:01 +0100

    v.0.1.4 pwa 8

--- resources/views/vendor/filachat/filachat/components/chat-box.blade.php
+++ resources/views/vendor/filachat/filachat/components/chat-box.blade.php
@@ -590,21 +590,7 @@
 
 
     console.log("i am working");
-  const firebaseConfig = {
-    apiKey: "AIzaSyCzz91VFPinYPTQ97Gjoq_lkGObCWib_88",
-    authDomain: "lbaraka-1f464.firebaseapp.com",
-    projectId: "lbaraka-1f464",
-    storageBucket: "lbaraka-1f464.firebasestorage.app",
-    messagingSenderId: "825065799200",
-    appId: "1:825065799200:web:e790fe16dc95fef0c50645",
-    measurementId: "G-JRRLJHPNCX"
-  };
 
-  firebase.initializeApp(firebaseConfig);
-
-  const messaging = firebase.messaging();
-
-  let currentToken = null; // Declare a global variable
 
 
 
@@ -642,14 +628,6 @@
     }
 
 
-  // Foreground notification handler
-  messaging.onMessage((payload) => {
-    console.log("Foreground message:", payload);
-    new Notification(payload.notification.title, {
-      body: payload.notification.body,
-    //   icon: '/icon.png'
-    });
-  });
 
 
 
