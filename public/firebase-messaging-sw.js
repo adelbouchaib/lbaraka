4ff1513efb74afa5169d3f28949625989f353695
Adel-Bouchaib <adelbouchaib@outlook.com>
2025-04-10 14:00:02 +0100

    v.0.1.4 pwa 14

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,64 +30,53 @@ messaging.onBackgroundMessage(function(payload) {
 
 
 
-console.log("i am working");
-const firebaseConfig = {
-  apiKey: "AIzaSyCzz91VFPinYPTQ97Gjoq_lkGObCWib_88",
-  authDomain: "lbaraka-1f464.firebaseapp.com",
-  projectId: "lbaraka-1f464",
-  storageBucket: "lbaraka-1f464.firebasestorage.app",
-  messagingSenderId: "825065799200",
-  appId: "1:825065799200:web:e790fe16dc95fef0c50645",
-  measurementId: "G-JRRLJHPNCX"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const messaging2 = firebase.messaging2();
-
-let currentToken = null; // Declare a global variable
+    console.log("i am working");
 
 
 
-// Ask permission and get token
-Notification.requestPermission().then((permission) => {
-  if (permission === "granted") {
-    messaging2.getToken({ vapidKey: 'BLX4N79hrhWKADdk6elMxsY9nijOccotAwR0mtsv00A8WtAtjK-LRqeR64uCLBNY0RlYCfVy8c5c0n3bnntfsiY' })
-      .then((currentToken) => {
-        if (currentToken) {
-          console.log("FCM Token:", currentToken);
-
-          // Send this token to your server
-          sendTokenToServer(currentToken); // Example: send the token to your server
-         
-         
-
-        } else {
-          console.log("No registration token available.");
-        }
-      }).catch((err) => {
-        console.error("Error getting token:", err);
-      });
-  } else {
-    console.warn("Permission not granted");
-  }
-});
+  let currentToken = null; // Declare a global variable
 
 
-// function sendTokenToServer(currentToken) {
-      
-//       // Call the Livewire method and pass the JavaScript variable
-//       @this.call('storeUserToken', currentToken);
-//       // window.livewire.emit('storeUserToken', currentToken);
 
-//   }
+  // Ask permission and get token
+  Notification.requestPermission().then((permission) => {
+    if (permission === "granted") {
+      messaging.getToken({ vapidKey: 'BLX4N79hrhWKADdk6elMxsY9nijOccotAwR0mtsv00A8WtAtjK-LRqeR64uCLBNY0RlYCfVy8c5c0n3bnntfsiY' })
+        .then((currentToken) => {
+          if (currentToken) {
+            console.log("FCM Token:", currentToken);
 
+            // Send this token to your server
+            // sendTokenToServer(currentToken); // Example: send the token to your server
+           
+           
 
-// Foreground notification handler
-messaging2.onMessage((payload) => {
-  console.log("Foreground message:", payload);
-  new Notification(payload.notification.title, {
-    body: payload.notification.body,
-  //   icon: '/icon.png'
+          } else {
+            console.log("No registration token available.");
+          }
+        }).catch((err) => {
+          console.error("Error getting token:", err);
+        });
+    } else {
+      console.warn("Permission not granted");
+    }
   });
-});
+
+
+//   function sendTokenToServer(currentToken) {
+        
+//         // Call the Livewire method and pass the JavaScript variable
+//         @this.call('storeUserToken', currentToken);
+//         // window.livewire.emit('storeUserToken', currentToken);
+
+//     }
+
+
+  // Foreground notification handler
+  messaging.onMessage((payload) => {
+    console.log("Foreground message:", payload);
+    new Notification(payload.notification.title, {
+      body: payload.notification.body,
+    //   icon: '/icon.png'
+    });
+  });
